072fad5f8d73e619ef666611caa1b29a3f7505f1
Saurabh Pandey <saurabhpandey4711@gmail.com>
2026-09-21 17:39:20 +0530

    Update frontend API URLs

--- frontend/src/services/uploadService.js
+++ frontend/src/services/uploadService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+   baseURL: `${import.meta.env.VITE_API_URL}/api`,
+
 });
 
 export const uploadImage = async (imageFile) => {
